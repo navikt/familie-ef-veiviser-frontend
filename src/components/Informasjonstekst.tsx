a90aea8cb5c89935de6fa2d21e99bad7e7b3a946
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-08-23 14:53:06 +0200

    Informasjonsbokser

--- src/components/Informasjonstekst.tsx
+++ src/components/Informasjonstekst.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import 'react-day-picker/lib/style.css';
+import Tekstomrade from 'nav-frontend-tekstomrade';
+import { Panel } from 'nav-frontend-paneler';
 import informasjonstekst from '../informasjonstekst.json';
 
 interface InformasjonstekstProps {
@@ -12,7 +14,19 @@ const Informasjonstekst: React.FC<InformasjonstekstProps> = ({ steg, alder, innt
 
     const infotekst = informasjonstekst.find((s) => s.svar_id === steg) || informasjonstekst[0];
 
-    return <div dangerouslySetInnerHTML={{__html: infotekst.body}} />;
+    return (
+        <>
+            <h2>{infotekst.header}</h2>
+            <h3>{infotekst.header2}</h3>
+            <div className="tekst-wrapper">
+                <Panel>
+                    <Tekstomrade>
+                        {`${infotekst.body}`}
+                    </Tekstomrade>
+                </Panel>
+            </div>
+        </>
+    );
 
 };
 
