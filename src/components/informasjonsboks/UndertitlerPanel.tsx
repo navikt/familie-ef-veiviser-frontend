a4b0a13d841bcb372bc140f85ebad7200ef471f8
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-09-30 15:28:40 +0200

    Sjekk at elementer i undertittel er til stede

--- src/components/informasjonsboks/UndertitlerPanel.tsx
+++ src/components/informasjonsboks/UndertitlerPanel.tsx
@@ -14,11 +14,15 @@ const UndertitlerPanel: React.FC<IUndertitlerPanelProps> = ({
       {undertitler.map((undertittel: IUndertittel, i: number) => {
         return (
           <div className="undertittel-panel" key={i}>
-            <h2>{undertittel.tekst_i_panel}</h2>
+            {undertittel.tekst_i_panel ? (
+              <h2>{undertittel.tekst_i_panel}</h2>
+            ) : null}
             {undertittel.brodtekster.map((brodtekst: any, i: number) => {
               return <MarkdownViewer key={i} markdown={brodtekst.body} />;
             })}
-            {undertittel.knapp ? (
+            {undertittel.knapp &&
+            undertittel.knapp.tekst &&
+            undertittel.knapp.lenke ? (
               <a href={undertittel.knapp.lenke} className="knapp">
                 {undertittel.knapp.tekst}
               </a>
