80346a192b9e1611e9ac05928e18457f2a2b5b35
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-10-15 13:09:18 +0200

    Add ID

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,7 +1,7 @@
 const sanityClient = require('@sanity/client');
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_ID,
-  dataset: process.env.REACT_APP_SANITY_DATASET,
+  projectId: '8wpntadz',
+  dataset: 'questions',
   useCdn: true,
 });
