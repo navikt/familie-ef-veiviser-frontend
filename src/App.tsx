344dcfe88b4f8ef51207d6fb303fddfa762a6c95
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-11-20 14:43:07 +0100

    Fiks polyfill

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { client } from './utils/sanity';
 import { Knapp } from 'nav-frontend-knapper';
 import { IInfoMapping, ISporsmal } from './models/Sporsmal';
 import { scrollTilRef } from './utils/utils';
-import 'react-app-polyfill/ie11';
 
 const App = () => {
   const [sporsmalListe, setSporsmalListe] = useState<ISporsmal[]>([]);
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/ie11';
+import 'utils/polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
