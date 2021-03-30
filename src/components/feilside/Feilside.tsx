7474646222e17c58b4b855f8eee4c0ed00de0d88
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-03-30 16:26:43 +0200

    Legg til styled-components

--- src/components/feilside/Feilside.tsx
+++ src/components/feilside/Feilside.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
+import styled from 'styled-components';
+
+const StyledFeilside = styled.div`
+  padding: 1;
+  width: 400px;
+  margin: auto;
+`;
 
 const Feilside = () => {
   return (
-    <div className="feilside">
+    <StyledFeilside>
       <AlertStripeFeil>Noe galt skjedde</AlertStripeFeil>
-    </div>
+    </StyledFeilside>
   );
 };
 
