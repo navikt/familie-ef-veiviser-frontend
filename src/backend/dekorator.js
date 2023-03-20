0ae4ce1e333437d0739a64651e91c35a6ba15fe9
Viktor <viktor.grondalen.solberg@nav.no>
2023-03-20 16:22:03 +0100

    skal hente dekoratør på annen måte. Fjerner request og mustache-express

---
+++ src/backend/dekorator.js
@@ -0,0 +1,21 @@
+const dekorator = require('@navikt/nav-dekoratoren-moduler/ssr');
+const path = require('path');
+
+const env = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
+
+const indexHandler = (_, res) => {
+  dekorator
+    .injectDecoratorServerSide({
+      env: env,
+      filePath: `${path.join(process.cwd(), 'build')}/index.html`,
+    })
+    .then((html) => {
+      res.send(html);
+    })
+    .catch((e) => {
+      console.log(e);
+      const error = `En feil oppstod. Klikk <a href='https://www.nav.no'>her</a> for å gå tilbake til nav.no. Kontakt kundestøtte hvis problemet vedvarer.`;
+      res.status(500).send(error);
+    });
+};
+module.exports = indexHandler;
