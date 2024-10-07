11e15db04dabb1f8b468fd5e85a483eabda77f76
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-10-07 10:17:36 +0200

    Sletter unødvendige/gamle dependencies. scroll-behavior: smooth har støtte i alle moderne brukte nettlesere.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import App from './App';
-import smoothscroll from 'smoothscroll-polyfill';
 import '@navikt/ds-css';
 import { createRoot } from 'react-dom/client';
-
-smoothscroll.polyfill();
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
