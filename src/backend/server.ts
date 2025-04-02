f16b4e2e9fa572ce764e61fc1e08075d9673687e
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-04-02 12:38:24 +0200

    Vil at server skal være typescript fil

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,5 @@
 import dekorator from '@navikt/nav-dekoratoren-moduler/ssr/index.js';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 
 const app = express();
@@ -8,16 +8,16 @@ const port = 8080;
 const BASE_PATH = '/familie/alene-med-barn/veiviser';
 const byggmappeFrontend = path.join(process.cwd(), 'dist');
 
-const indexHandler = (_, res) => {
+const indexHandler = (_: Request, res: Response): void => {
   dekorator
     .injectDecoratorServerSide({
       env: process.env.ENV === 'production' ? 'prod' : 'dev',
       filePath: `${path.join(process.cwd(), 'dist')}/index.html`,
     })
-    .then((html) => {
+    .then((html: string) => {
       res.send(html);
     })
-    .catch((e) => {
+    .catch((e: Error) => {
       console.log(e);
       const error = `En feil oppstod. Klikk <a href='https://www.nav.no'>her</a> for å gå tilbake til nav.no. Kontakt kundestøtte hvis problemet vedvarer.`;
       res.status(500).send(error);
@@ -26,7 +26,7 @@ const indexHandler = (_, res) => {
 
 app.set('views', byggmappeFrontend);
 
-app.get(`${BASE_PATH}/status`, (_req, res) => {
+app.get(`${BASE_PATH}/status`, (_req: Request, res: Response) => {
   res.status(200).end();
 });
 
